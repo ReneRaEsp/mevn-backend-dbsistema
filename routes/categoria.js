802fceeeb13b58e667bf0fec92fb9c9c994ea431
Rene <rdejramirez@gmail.com>
2021-06-17 13:37:56 -0500

    backend operativo

--- routes/categoria.js
+++ routes/categoria.js
@@ -6,26 +6,19 @@ import auth from '../middlewares/auth'
 const router=routerx();
 
 
-//router.post('/add', auth.verifyAlmacenero, categoriaController.add)
-router.post('/add', categoriaController.add)
+router.post('/add', auth.verifyAlmacenero, categoriaController.add)
 
-//router.get('/query', auth.verifyAlmacenero, categoriaController.query)
-router.get('/query', categoriaController.query)
+router.get('/query', auth.verifyAlmacenero, categoriaController.query)
 
-//router.get('/list', auth.verifyAlmacenero, categoriaController.list)
-router.get('/list', categoriaController.list)
+router.get('/list', auth.verifyAlmacenero, categoriaController.list)
 
-//router.put('/update', auth.verifyAlmacenero, categoriaController.update)
-router.put('/update', categoriaController.update)
+router.put('/update', auth.verifyAlmacenero, categoriaController.update)
 
-//router.delete('/remove', auth.verifyAlmacenero, categoriaController.remove)
-router.delete('/remove', categoriaController.remove)
+router.delete('/remove', auth.verifyAlmacenero, categoriaController.remove)
 
-//router.put('/activate', auth.verifyAlmacenero, categoriaController.activate)
-router.put('/activate', categoriaController.activate)
+router.put('/activate', auth.verifyAlmacenero, categoriaController.activate)
 
-//router.put('/deactivate', auth.verifyAlmacenero,  categoriaController.deactivate)
-router.put('/deactivate', categoriaController.deactivate)
+router.put('/deactivate', auth.verifyAlmacenero,  categoriaController.deactivate)
 
 
 export default router;
