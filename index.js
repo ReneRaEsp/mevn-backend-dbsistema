bf8879737f62d61498a1cf6e79202c55136771fa
ReneRaEsp <rdejramirez@gmail.com>
2021-07-22 01:32:27 -0500

    frontend agregado y conexion a base de datos mongo atlas completada

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import router from './routes'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./routes";
 //const express = require('express')
 //const morgan = require('morgan')
 //const cors=require('cors')
 //Conexion a la base de datos MongoDB
-mongoose.Promise=global.Promise
-const dbUrl = 'mongodb://localhost/dbsistema'
-//const dbUrl = 'mongodb://127.0.0.1:27017/dbsistema'
+mongoose.Promise = global.Promise;
+//const dbUrl = 'mongodb://localhost/dbsistema';
+const dbUrl =
+  "mongodb+srv://reneraesp:Autopropio0.@cluster0.cazyv.mongodb.net/dbsistema?retryWrites=true&w=majority";
 
-mongoose.connect(dbUrl, {useCreateIndex:true, useNewUrlParser: true, useUnifiedTopology: true})
-.then(mongoose => console.log(`conectado a la BD en el puerto 27017`))
-.catch(err => console.log(err))
+mongoose
+  .connect(dbUrl, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((mongoose) => console.log(`conectado a la BD en el puerto 27017`))
+  .catch((err) => console.log(err));
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000
-app.use(morgan('dev'))
-app.use(cors())
+const PORT = process.env.PORT || 5000;
+app.use(morgan("dev"));
+app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
-app.use('/api',router)
+app.use("/api", router);
 
-app.listen(PORT,()=>{
-    console.log(`Escuchando desde el puerto ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Escuchando desde el puerto ${PORT}`);
+});
---
+++ public/precache-manifest.dbb20c8a9a5ed885fb14219011ee464c.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0b3a2f10080d528807",
+    "url": "css/accesos.b80037d5.css"
+  },
+  {
+    "revision": "c4bc233ad0658cd562de",
+    "url": "css/acercaDe.0393bf73.css"
+  },
+  {
+    "revision": "16d036be8549e7a3d502",
+    "url": "css/agregar-categorias.273c0e3d.css"
+  },
+  {
+    "revision": "56b7b92b3745709e4175",
+    "url": "css/almacen-articulos.75accb63.css"
+  },
+  {
+    "revision": "89a78c661d7a922cd0b8",
+    "url": "css/almacen-categorias.c8233c45.css"
+  },
+  {
+    "revision": "a59e7e9c4768e1746d86",
+    "url": "css/app.e80ffabf.css"
+  },
+  {
+    "revision": "ac66ab108255abffc417",
+    "url": "css/clientes.75468a10.css"
+  },
+  {
+    "revision": "44d0be361ff8dcbd1dae",
+    "url": "css/consultas-compras.4ce43dc9.css"
+  },
+  {
+    "revision": "782d376d1f0670fffe16",
+    "url": "css/consultas-compras~consultas-ventas.c1123cea.css"
+  },
+  {
+    "revision": "95059d006ae0d03c5e08",
+    "url": "css/consultas-ventas.efc60fa0.css"
+  },
+  {
+    "revision": "84cfc2a9bdc7fa307d09",
+    "url": "css/detalles-ingresos.241fdd6c.css"
+  },
+  {
+    "revision": "7dded196601db81f383c",
+    "url": "css/detalles-ventas.1417cddf.css"
+  },
+  {
+    "revision": "2f89729b59e5f5de3acd",
+    "url": "css/editar-articulos.0af8632b.css"
+  },
+  {
+    "revision": "3ba4e741185c1f86a6ba",
+    "url": "css/editar-clientes.6e9c0829.css"
+  },
+  {
+    "revision": "5fb921b0505bff2fa462",
+    "url": "css/editar-ingresos.ccbe33e7.css"
+  },
+  {
+    "revision": "847b0e32e4578248fd0c",
+    "url": "css/editar-proveedores.c5c7cc1e.css"
+  },
+  {
+    "revision": "90a0c88074fdda65a754",
+    "url": "css/editar-usuario.7973b39d.css"
+  },
+  {
+    "revision": "597eea5fd8442403553e",
+    "url": "css/editar-ventas.908f7684.css"
+  },
+  {
+    "revision": "7a3225f649d8b5ec448e",
+    "url": "css/ingresos.155a73eb.css"
+  },
+  {
+    "revision": "d04efdaeec8b28d50216",
+    "url": "css/inicio.664e0990.css"
+  },
+  {
+    "revision": "d6a1646c645a4794f81b",
+    "url": "css/login.7d6a19be.css"
+  },
+  {
+    "revision": "ffdc2f47b0a8a96d8378",
+    "url": "css/proveedores.56930c84.css"
+  },
+  {
+    "revision": "3acf89396ae6ab63319d",
+    "url": "css/ventas.4336080f.css"
+  },
+  {
+    "revision": "ea3898d7343f53f9d5157f9e6201e180",
+    "url": "img/fondo.ea3898d7.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4e05592a3db2dfdd277718f4a05e9d7a",
+    "url": "index.html"
+  },
+  {
+    "revision": "8d0b3a2f10080d528807",
+    "url": "js/accesos.afd90818.js"
+  },
+  {
+    "revision": "c4bc233ad0658cd562de",
+    "url": "js/acercaDe.8bfdb016.js"
+  },
+  {
+    "revision": "16d036be8549e7a3d502",
+    "url": "js/agregar-categorias.50184240.js"
+  },
+  {
+    "revision": "56b7b92b3745709e4175",
+    "url": "js/almacen-articulos.8b968837.js"
+  },
+  {
+    "revision": "89a78c661d7a922cd0b8",
+    "url": "js/almacen-categorias.eb6e210c.js"
+  },
+  {
+    "revision": "a59e7e9c4768e1746d86",
+    "url": "js/app.8e3226a0.js"
+  },
+  {
+    "revision": "f8fdb7bf7733c14dd3b9",
+    "url": "js/chunk-vendors.b1c3f795.js"
+  },
+  {
+    "revision": "ac66ab108255abffc417",
+    "url": "js/clientes.4a97d575.js"
+  },
+  {
+    "revision": "44d0be361ff8dcbd1dae",
+    "url": "js/consultas-compras.dc71c54c.js"
+  },
+  {
+    "revision": "782d376d1f0670fffe16",
+    "url": "js/consultas-compras~consultas-ventas.8dc3200c.js"
+  },
+  {
+    "revision": "95059d006ae0d03c5e08",
+    "url": "js/consultas-ventas.b6a0efd8.js"
+  },
+  {
+    "revision": "84cfc2a9bdc7fa307d09",
+    "url": "js/detalles-ingresos.7d2d9679.js"
+  },
+  {
+    "revision": "7dded196601db81f383c",
+    "url": "js/detalles-ventas.632b649a.js"
+  },
+  {
+    "revision": "2f89729b59e5f5de3acd",
+    "url": "js/editar-articulos.985850bc.js"
+  },
+  {
+    "revision": "3ba4e741185c1f86a6ba",
+    "url": "js/editar-clientes.4b65da8d.js"
+  },
+  {
+    "revision": "5fb921b0505bff2fa462",
+    "url": "js/editar-ingresos.ebea5dfc.js"
+  },
+  {
+    "revision": "847b0e32e4578248fd0c",
+    "url": "js/editar-proveedores.b1779f16.js"
+  },
+  {
+    "revision": "90a0c88074fdda65a754",
+    "url": "js/editar-usuario.0d454d18.js"
+  },
+  {
+    "revision": "597eea5fd8442403553e",
+    "url": "js/editar-ventas.3947b655.js"
+  },
+  {
+    "revision": "7a3225f649d8b5ec448e",
+    "url": "js/ingresos.aec4df36.js"
+  },
+  {
+    "revision": "d04efdaeec8b28d50216",
+    "url": "js/inicio.bc07e2f1.js"
+  },
+  {
+    "revision": "d6a1646c645a4794f81b",
+    "url": "js/login.1bd1c619.js"
+  },
+  {
+    "revision": "ffdc2f47b0a8a96d8378",
+    "url": "js/proveedores.df799406.js"
+  },
+  {
+    "revision": "3acf89396ae6ab63319d",
+    "url": "js/ventas.5710d08f.js"
+  },
+  {
+    "revision": "39e2b679f778be78a05734bd47ceac69",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
