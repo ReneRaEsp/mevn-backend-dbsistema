c5b0d66ae437b08ee69ee4e38532bb9fb1f5d240
Rene Ramirez <rdejramirez@gmail.com>
2022-07-18 00:26:45 -0500

    refactoring index.js

--- index.js
+++ index.js
@@ -3,37 +3,41 @@ import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
 import router from "./routes";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+//Variables de entorno
+const DB_URL = process.env.DB_URL;
+const PORT = process.env.PORT || 5000;
 
 //Conexion a la base de datos MongoDB
 mongoose.Promise = global.Promise;
-//const dbUrl = 'mongodb://localhost/dbsistema';
-const dbUrl =
-  "mongodb+srv://reneraesp:Autopropio0.@cluster0.cazyv.mongodb.net/dbsistema?retryWrites=true&w=majority";
 
 mongoose
-  .connect(dbUrl, {
+  .connect(DB_URL, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then((mongoose) => console.log(`conectado a la BD en el puerto: ${mongoose}`))
+  .then(() => console.log(`conectado a la BD`))
   .catch((err) => console.log(err));
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
 app.use(morgan("dev"));
 app.use(cors());
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
